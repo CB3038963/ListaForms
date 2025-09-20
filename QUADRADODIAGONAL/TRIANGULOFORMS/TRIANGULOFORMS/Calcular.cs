b936e4440a73c9320d05069ea4f35663e77745c2
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-20 20:55:13 -0300

    Add files via upload

---
+++ QUADRADODIAGONAL/TRIANGULOFORMS/TRIANGULOFORMS/Calcular.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRIANGULOFORMS
+{
+    internal class Calcular
+    {
+        private double baze, h, a;
+        public Calcular() 
+        {
+            this.baze = 0;
+            this.h = 0;
+            this.a = 0;
+        }
+        public Calcular(double baze, double h)
+        {
+            this.baze = baze;
+            this.h = h;
+        }
+        public void setBase(double baze)
+        {this.baze = baze;   }
+        public void setH(double h)
+        { this.h = h; }
+
+        public double getArea()
+        { return this.a; }
+        
+        public void calcular()
+        {
+            this.a = this.h * this.baze;
+        }
+
+    }
+}
