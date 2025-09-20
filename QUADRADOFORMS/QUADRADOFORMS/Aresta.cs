8d24dbb0ac7d38664b2e76edaec8dc17c251db1f
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-20 20:27:14 -0300

    Add files via upload

---
+++ QUADRADOFORMS/QUADRADOFORMS/Aresta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUADRADOFORMS
+{
+    internal class Aresta
+    {
+        private double aresta, area;
+        
+        public Aresta()
+        {
+            this.aresta = 0;
+            this.area = 0;
+        }
+        public Aresta(double aresta)
+        {
+            this.aresta = aresta;  
+        }
+        public void setAresta(double aresta)
+        {
+            this.aresta = aresta;
+        }
+        public double getArea()
+        {
+            return this.area;
+        }
+        public void calcular()
+        {
+            this.area = Math.Pow(this.aresta,2);
+        }
+
+    }
+}
---
+++ QUADRADOFORMS/QUADRADOFORMS/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace QUADRADOFORMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArea = new System.Windows.Forms.Button();
+            this.txtAresta = new System.Windows.Forms.TextBox();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnArea
+            // 
+            this.btnArea.Location = new System.Drawing.Point(300, 199);
+            this.btnArea.Name = "btnArea";
+            this.btnArea.Size = new System.Drawing.Size(75, 23);
+            this.btnArea.TabIndex = 0;
+            this.btnArea.Text = "CALCULAR AREA";
+            this.btnArea.UseVisualStyleBackColor = true;
+            this.btnArea.Click += new System.EventHandler(this.btnArea_Click);
+            // 
+            // txtAresta
+            // 
+            this.txtAresta.Location = new System.Drawing.Point(300, 104);
+            this.txtAresta.Name = "txtAresta";
+            this.txtAresta.Size = new System.Drawing.Size(100, 20);
+            this.txtAresta.TabIndex = 1;
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.BackColor = System.Drawing.Color.Lime;
+            this.lblArea.Location = new System.Drawing.Point(300, 245);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(0, 13);
+            this.lblArea.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ARESTA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "RESULTADO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.txtAresta);
+            this.Controls.Add(this.btnArea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArea;
+        private System.Windows.Forms.TextBox txtAresta;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
