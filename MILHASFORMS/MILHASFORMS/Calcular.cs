858b55e88e55ba8cd1d7e6e0ec2a9f6e5dd42b5b
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-20 21:06:57 -0300

    Add files via upload

---
+++ MILHASFORMS/MILHASFORMS/Calcular.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MILHASFORMS
+{
+    internal class Calcular
+    {
+        private double milha, metros;
+        private double maritima = 1852;
+        public Calcular() { this.milha = 0; }
+        public Calcular (double milha) { this.milha = milha; }
+        public void setMilha (double milha) {this.milha = milha; }
+        public double getMetro() { return this.metros; }
+        public void calcular() { this.metros =  this.milha * this.maritima; }
+    }
+}
---
+++ MILHASFORMS/MILHASFORMS/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("MILHASFORMS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MILHASFORMS")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("2c1d3463-abf0-4f83-84b3-ad9b4a26b9c2")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
