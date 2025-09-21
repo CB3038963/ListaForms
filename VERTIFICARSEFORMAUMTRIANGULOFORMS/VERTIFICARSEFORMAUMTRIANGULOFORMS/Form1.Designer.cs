042fd00422bcad55b5fc8b143cc296863c7b59c2
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-20 23:14:48 -0300

    Add files via upload

---
+++ VERTIFICARSEFORMAUMTRIANGULOFORMS/VERTIFICARSEFORMAUMTRIANGULOFORMS/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace VERTIFICARSEFORMAUMTRIANGULOFORMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(240, 271);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 0;
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Location = new System.Drawing.Point(243, 52);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoA.TabIndex = 1;
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Location = new System.Drawing.Point(243, 88);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoB.TabIndex = 2;
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Location = new System.Drawing.Point(243, 123);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoC.TabIndex = 3;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(243, 189);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "VERIFICAR";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "LADO A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "LADO B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "LADO C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.txtLadoA);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ VERTIFICARSEFORMAUMTRIANGULOFORMS/VERTIFICARSEFORMAUMTRIANGULOFORMS/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VERTIFICARSEFORMAUMTRIANGULOFORMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            Verificando v = new Verificando(double.Parse(txtLadoA.Text), double.Parse(txtLadoB.Text), double.Parse(txtLadoC.Text));
+            v.resolucao();
+
+            if (double.Parse(txtLadoA.Text) < double.Parse(txtLadoB.Text) + double.Parse(txtLadoC.Text) &&
+               double.Parse(txtLadoB.Text) < double.Parse(txtLadoA.Text) + double.Parse(txtLadoC.Text) &&
+               double.Parse(txtLadoC.Text) < double.Parse(txtLadoA.Text) + double.Parse(txtLadoB.Text))
+
+            {
+                if (double.Parse(txtLadoA.Text) == double.Parse(txtLadoB.Text) &&
+                   double.Parse(txtLadoA.Text) == double.Parse(txtLadoC.Text))
+                { lblResultado.Text = "FORMA UM TRIANGULO EQUILATERO"; }
+
+                if (double.Parse(txtLadoA.Text) != double.Parse(txtLadoB.Text) &&
+                   double.Parse(txtLadoA.Text) != double.Parse(txtLadoC.Text) &&
+                   double.Parse(txtLadoB.Text) != double.Parse(txtLadoC.Text))
+                {
+                    lblResultado.Text = "FORMA UM TRIANGULO ESCALENO";
+                }
+
+                if (double.Parse(txtLadoA.Text) == double.Parse(txtLadoB.Text) && double.Parse(txtLadoA.Text) != double.Parse(txtLadoC.Text) ||
+                   double.Parse(txtLadoB.Text) == double.Parse(txtLadoC.Text) && double.Parse(txtLadoC.Text) != double.Parse(txtLadoA.Text) ||
+                   double.Parse(txtLadoC.Text) == double.Parse(txtLadoA.Text) && double.Parse(txtLadoA.Text) != double.Parse(txtLadoB.Text))
+                {
+                    lblResultado.Text = "FORMA UM TRIANGULO ISOSCELES";
+                }
+
+            }
+
+            else { lblResultado.Text = "ESSES VALORES NAO FORMAM UM TRIANGULO"; }
+
+        }
+    }
+}
