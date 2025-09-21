9b338fb939ab1ea172b727e75f1f3af9f76b91a0
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-20 21:14:28 -0300

    Add files via upload

---
+++ DOLARFORMS/DOLARFORMS/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOLARFORMS
+{
+    internal class Converter
+    {
+        private double dolar, real;
+        public Converter() { this.dolar = 0; }
+        public Converter(double dolar) { this.dolar = dolar; }
+        public void setDolar(double dolar) { this.dolar = dolar; }
+        public double getReal() { return this.real; }
+        public void converter() { this.real = this.dolar * 5; }
+    }
+}
