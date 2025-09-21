eb2054f81b71b8a6a1a38bafaef081b8f8db713e
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-20 22:39:16 -0300

    Add files via upload

---
+++ PESOALTURAFORMS/PESOALTURAFORMS/Calcular.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PESOALTURAFORMS
+{
+    internal class Calcular
+    {
+        private double peso, altura, resultado;
+        public Calcular() { this.peso = 0; this.altura = 0; }
+        public Calcular(double peso, double altura) { this.peso = peso; this.altura = altura; }
+        public void setPeso(double peso) { this.peso = peso; }  
+        public void setAltura(double altura) { this.altura = altura; }  
+        public double getResultado() { return this.resultado; }
+        public void calculando() { this.resultado = this.peso / (Math.Pow(this.altura, 2)); }
+    }
+}
---
+++ PESOALTURAFORMS/PESOALTURAFORMS/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PESOALTURAFORMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Calcular ideal = new Calcular(double.Parse(txtPeso.Text),double.Parse(txtAltura.Text));
+            ideal.calculando();
+            if (ideal.getResultado() < 20) { lblResultado.Text = "ABAIXO DO PESO"; }
+            else if (ideal.getResultado() >= 25) { lblResultado.Text = "ACIMA DO PESO"; }
+            else { lblResultado.Text = "PESO IDEAL"; }
+        }
+    }
+}
