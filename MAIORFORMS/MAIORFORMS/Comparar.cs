b66a4fa4ab21da97c33dbea7647b34b9338bdd12
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-20 21:25:30 -0300

    Add files via upload

---
+++ MAIORFORMS/MAIORFORMS/Comparar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIORFORMS
+{
+    internal class Comparar
+    {
+        private double n1, n2, maior;
+        public Comparar()
+        {
+            this.n1 = 0;
+            this.n2 = 0;
+        }
+        public Comparar(double n1 ,double n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+        public void setN1(double n1) {this.n1 = n1;}    
+        public void setN2(double n2) {this.n2 = n2;}
+
+        public double getMaior() { return this.maior; }
+
+        public void calculando()
+        {
+            if (this.n1 > this.n2) { this.maior = this.n1; }
+            else { this.maior = this.n2; }
+        }
+    }
+}
---
+++ MAIORFORMS/MAIORFORMS/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MAIORFORMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComparar_Click(object sender, EventArgs e)
+        {
+            Comparar comparar = new Comparar(double.Parse(txtN1.Text),double.Parse(txtN2.Text));
+            comparar.calculando();
+            lblMaior.Text = comparar.getMaior().ToString();
+        }
+    }
+}
